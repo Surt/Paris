ac726b8be80b4562215d06ff94f54270b9507ccd
Alexander Grigorev <visavi.net@mail.ru>
2017-01-29 15:37:46 +0300

    Update ORM.php
    fix bug - Use of undefined constant self - assumed 'self'

--- src/Granada/ORM.php
+++ src/Granada/ORM.php
@@ -2182,6 +2182,6 @@ class ORM implements ArrayAccess {
     {
         $method = strtolower(preg_replace('/([a-z])([A-Z])/', '$1_$2', $name));
 
-        return call_user_func_array(array(self, $method), $arguments);
+        return call_user_func_array(array('self', $method), $arguments);
     }
 }
